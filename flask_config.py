b23b378dc5f6cba50e655b3fe8483d13246c9e29
Hugh Emerson <Hugh.Emerson@softwire.com>
2022-05-23 14:45:49 +0100

    Trust self-signed cert for local sql setup

--- flask_config.py
+++ flask_config.py
@@ -4,7 +4,7 @@ import urllib.parse
 
 valid_drivers = ['ODBC Driver 17 for SQL Server']
 driver = next((driver for driver in pyodbc.drivers() if driver in valid_drivers), pyodbc.drivers()[-1])
-connect_string = f"Driver={{{driver}}};Server=tcp:{os.environ.get('DB_SERVER_NAME')},1433;Database={os.environ.get('DATABASE_NAME')};Uid={os.environ.get('DATABASE_USER')};Pwd={os.environ.get('DATABASE_PASSWORD')};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
+connect_string = f"Driver={{{driver}}};Server=tcp:{os.environ.get('DB_SERVER_NAME')},1433;Database={os.environ.get('DATABASE_NAME')};Uid={os.environ.get('DATABASE_USER')};Pwd={os.environ.get('DATABASE_PASSWORD')};Encrypt=yes;TrustServerCertificate=Yes;Connection Timeout=30;"
 url_encoded_connect_string = urllib.parse.quote_plus(connect_string)
 
 class Config:
